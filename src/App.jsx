73fcdafbedd6e002bfe8c9db3c8a92797be25374
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2024-01-05 19:46:14 +0630

    create form done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Container from "./components/Container";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import CreateForm from "./components/CreateForm";
 
 const App = () => {
   return (
     <Container>
-      {/* <Login /> */}
-      <Register />
+      <CreateForm />
     </Container>
   );
 };
